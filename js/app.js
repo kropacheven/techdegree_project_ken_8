546462ddd0d8d06009c5e700398d17b1d62e3ae7
kropacheven <81347649+kropacheven@users.noreply.github.com>
2021-12-04 19:06:39 +0800

    Update2_04_12_2021
    Adding global variables to JS file

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// global variables
+let employees = [];
+const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
+email, location, phone, dob &noinfo &nat=US`
+const gridContainer = document.querySelector(".grid-container");
+const overlay = document.querySelector(".overlay");
+const modalContainer = document.querySelector(".modal-content");
+const modalClose = document.querySelector(".modal-close");
+
+// fetch data from API
+fetch(urlAPI)
+.then(res => res.json())
+.then(res => res.results)
+.then(displayEmployees)
+.catch(err => console.log(err))
