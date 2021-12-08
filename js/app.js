949693e31a0fa936620ff2701a604a6eb5c4ae55
kropacheven <81347649+kropacheven@users.noreply.github.com>
2021-12-08 21:32:53 +0800

    Update_08_12_2021
    Adding display employee function

--- js/app.js
+++ js/app.js
@@ -13,3 +13,30 @@ fetch(urlAPI)
 .then(res => res.results)
 .then(displayEmployees)
 .catch(err => console.log(err))
+
+
+/*
+function displayEmployees(employeeData) {
+    employees = employeeData;
+    // store the employee HTML as we create it
+    let employeeHTML = '';
+    // loop through each employee and create HTML markup
+    employees.forEach((employee, index) => {
+    let name = employee.name;
+    let email = employee.email;
+    let city = employee.location.city;
+    let picture = employee.picture;
+    // template literals make this so much cleaner!
+    employeeHTML += `
+    <div class="card" data-index="${index}">
+    <img class="avatar" src="${picture.large}" />
+    <div class="text-container">
+    <h2 class="name">${name.first} ${name.last}</h2>
+    <p class="email">${email}</p>
+    <p class="address">${city}</p>
+    </div>
+    </div>
+    `
+});
+
+*/
