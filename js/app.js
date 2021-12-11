8aa43ae806f9eb1be94803db697ae964456ae568
kropacheven <81347649+kropacheven@users.noreply.github.com>
2021-12-11 22:52:02 +0800

    Update_11_12_2021
    Pasting in other functions

--- js/app.js
+++ js/app.js
@@ -40,5 +40,43 @@ function displayEmployees(employeeData) {
 });
 
 gridContainer.innerHTML = employeeHTML;
-Techdegree
 }
+
+
+function displayModal(index) {
+    // use object destructuring make our template literal cleaner
+    let { name, dob, phone, email, location: { city, street, state, postcode
+    }, picture } = employees[index];
+    let date = new Date(dob.date);
+    const modalHTML = `
+    <img class="avatar" src="${picture.large}" />
+    <div class="text-container">
+    <h2 class="name">${name.first} ${name.last}</h2>
+    <p class="email">${email}</p>
+    <p class="address">${city}</p>
+    <hr />
+    <p>${phone}</p>
+    <p class="address">${street}, ${state} ${postcode}</p>
+    <p>Birthday:
+    ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+    </div>
+    `;
+    overlay.classList.remove("hidden");
+    modalContainer.innerHTML = modalHTML;
+}
+
+
+gridContainer.addEventListener('click', e => {
+    // make sure the click is not on the gridContainer itself
+    if (e.target !== gridContainer) {
+    // select the card element based on its proximity to actual element
+    clicked
+    const card = e.target.closest(".card");
+    const index = card.getAttribute('data-index');
+    displayModal(index);
+    }
+});
+
+modalClose.addEventListener('click', () => {
+    overlay.classList.add("hidden");
+});
