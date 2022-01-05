ef7d87b51e809b9fa106a5a5ff40731b879c3bbf
kropacheven <81347649+kropacheven@users.noreply.github.com>
2022-01-05 23:23:19 +0800

    Update_05_01_2022
    Commenting out divs and function for later use

--- js/app.js
+++ js/app.js
@@ -65,17 +65,17 @@ function displayModal(index) {
 }
 
 
-gridContainer.addEventListener('click', e => {
-    // make sure the click is not on the gridContainer itself
-    if (e.target !== gridContainer) {
-    // select the card element based on its proximity to actual element
-    clicked
-    const card = e.target.closest(".card");
-    const index = card.getAttribute('data-index');
-    displayModal(index);
-    }
-});
+// gridContainer.addEventListener('click', e => {
+//     // make sure the click is not on the gridContainer itself
+//     if (e.target !== gridContainer) {
+//     // select the card element based on its proximity to actual element
+//     clicked
+//     const card = e.target.closest(".card");
+//     const index = card.getAttribute('data-index');
+//     displayModal(index);
+//     }
+// });
 
-modalClose.addEventListener('click', () => {
-    overlay.classList.add("hidden");
-});
+// modalClose.addEventListener('click', () => {
+//     overlay.classList.add("hidden");
+// });
