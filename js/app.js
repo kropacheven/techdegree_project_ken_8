60a6ef38be57f25ca3a596ae68af148707e7a7e6
kropacheven <81347649+kropacheven@users.noreply.github.com>
2022-01-06 12:50:12 +0800

    Update_06_01_2022
    Adding script tag in index.html file, making fetch request and function of displaying employees to run. Now employees are displayed on the page.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // global variables
 let employees = [];
-const urlAPI = `https://randomuser.me/api/?results=12&inc=name,picture,email,location,phone,dob &noinfo &nat=US`
+const urlAPI = `https://randomuser.me/api/?results=12&inc=name,picture,email,location,phone,dob&noinfo&nat=US`
 const gridContainer = document.querySelector(".grid-container");
 const overlay = document.querySelector(".overlay");
 const modalContainer = document.querySelector(".modal-content");
@@ -8,10 +8,11 @@ const modalClose = document.querySelector(".modal-close");
 
 // fetch data from API
 fetch(urlAPI)
-.then(res => res.json())
-.then(res => res.results)
-.then(displayEmployees)
-.catch(err => console.log(err))
+    .then(res => res.json())
+ //   .then((res) => console.log(res))
+    .then(res => res.results)
+    .then(res => displayEmployees(res))
+    .catch(err => console.log(err))
 
 
 
@@ -42,27 +43,27 @@ gridContainer.innerHTML = employeeHTML;
 }
 
 
-function displayModal(index) {
-    // use object destructuring make our template literal cleaner
-    let { name, dob, phone, email, location: { city, street, state, postcode
-    }, picture } = employees[index];
-    let date = new Date(dob.date);
-    const modalHTML = `
-    <img class="avatar" src="${picture.large}" />
-    <div class="text-container">
-    <h2 class="name">${name.first} ${name.last}</h2>
-    <p class="email">${email}</p>
-    <p class="address">${city}</p>
-    <hr />
-    <p>${phone}</p>
-    <p class="address">${street}, ${state} ${postcode}</p>
-    <p>Birthday:
-    ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
-    </div>
-    `;
-    overlay.classList.remove("hidden");
-    modalContainer.innerHTML = modalHTML;
-}
+// function displayModal(index) {
+//     // use object destructuring make our template literal cleaner
+//     let { name, dob, phone, email, location: { city, street, state, postcode
+//     }, picture } = employees[index];
+//     let date = new Date(dob.date);
+//     const modalHTML = `
+//     <img class="avatar" src="${picture.large}" />
+//     <div class="text-container">
+//     <h2 class="name">${name.first} ${name.last}</h2>
+//     <p class="email">${email}</p>
+//     <p class="address">${city}</p>
+//     <hr />
+//     <p>${phone}</p>
+//     <p class="address">${street}, ${state} ${postcode}</p>
+//     <p>Birthday:
+//     ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+//     </div>
+//     `;
+//     overlay.classList.remove("hidden");
+//     modalContainer.innerHTML = modalHTML;
+// }
 
 
 // gridContainer.addEventListener('click', e => {
